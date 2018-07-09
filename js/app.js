d862e6167ce7646d1b87cd78124fbe7d6021bcff
julius kamau <julius.kamau@zilojo.com>
2018-07-09 19:39:02 +0300

    layout styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+
+    var bLazy = new Blazy({
+        breakpoints: [{
+	    width: 420 // Max-width
+          , src: 'data-src-small'
+	}]
+      , success: function(element){
+	    setTimeout(function(){
+		// We want to remove the loader gif now.
+		// First we find the parent container
+		// then we remove the "loading" class which holds the loader image
+		var parent = element.parentNode;
+		parent.className = parent.className.replace(/\bloading\b/,'');
+	    }, 200);
+        }
+   });
+ 
+   $(document).ready(function(){
+    $("ul.nav .drop-link").click(function(){
+         $("ul.nav  li ul.drop-menu").toggle();
+     });
+ }); 
+
 $(document).foundation()
+
+
