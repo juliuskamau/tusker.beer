ad369f3a5fbab03a6733fd5e1b74bbb2e5f04037
julius kamau <julius.kamau@zilojo.com>
2018-07-19 10:40:11 +0300

    adde files
    added malt on the site

--- js/app.js
+++ js/app.js
@@ -15,11 +15,116 @@
         }
    });
  
+
+
    $(document).ready(function(){
     $("ul.nav .drop-link").click(function(){
          $("ul.nav  li ul.drop-menu").toggle();
      });
  }); 
+
+
+ function reveal() {
+    // window sizes
+    var winsize = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+    // if animating return
+    if (this.isAnimating) {
+        return false;
+    }
+    this.isAnimating = true;
+    var widthVal, heightVal, transform;
+    var pageDiagonal = Math.sqrt(Math.pow(winsize.width, 2) + Math.pow(winsize.height, 2));
+    widthVal = heightVal = pageDiagonal + 'px';
+    transform = 'translate3d(-50%,-50%,0) rotate3d(0,0,1,45deg) translate3d(0,' + pageDiagonal + 'px,0)';
+    var revealerWrapper = document.getElementById("revealer");
+    revealerWrapper.style.width = widthVal;
+    revealerWrapper.style.height = heightVal;
+    revealerWrapper.style.WebkitTransform = revealerWrapper.style.transform = transform;
+    revealerWrapper.style.opacity = 1;
+    setTimeout(function() {
+        revealerWrapper.style.opacity = 0;
+    }, 1600);
+    setTimeout(function() {
+        $("#loader-wrapper").fadeOut();
+    }, 750);
+}
+$(window).ready(function() {
+    if ($('body').hasClass("main")) {
+        reveal();
+    }
+    $("body").addClass("loaded");
+
+});
+
+
+(function ($) {
+
+
+// General Settings
+window.sr = ScrollReveal({ reset: false });
+
+// Custom Settings
+
+sr.reveal('.widgets-panels .large-6:nth-child(2)', { 
+  origin: 'right', 
+  duration: 1200,
+  scale: 1,
+  distance: '200px',
+  useDelay: 'once',
+  easing     : 'ease-out'
+});
+
+sr.reveal('.widgets-panels .large-6:nth-child(1)', { 
+  origin: 'left', 
+  duration: 1200,
+  scale: 1,
+  distance: '200px',
+  useDelay: 'once',
+  easing     : 'ease-out'
+});
+
+sr.reveal('.widgets-panels .large-6:nth-child(3)', { 
+  origin: 'bottom', 
+  duration: 1200,
+  scale: 1,
+  distance: '200px',
+  useDelay: 'once',
+  easing     : 'ease-out'
+});
+
+sr.reveal('.widgets-panels .large-6:nth-child(4)', { 
+  origin: 'bottom', 
+  duration: 1200,
+  scale: 1,
+  distance: '200px',
+  useDelay: 'once',
+  easing     : 'ease-out'
+});
+
+
+ sr.reveal('.banner h1', { 
+  origin   : "bottom",
+        distance : "64px",
+        duration : 900,
+        delay    : 1500,
+        scale    : 1,
+        easing     : 'ease-out'
+});  
+
+
+
+})(jQuery);
+
+
+
+
+
+
+
+
 
 $(document).foundation()
 
